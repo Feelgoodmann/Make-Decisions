2d26226a380724006b1081cc21e1020a035197cc
Feelgoodmann <tanapat.ch@kkumail.com>
2023-01-08 18:45:46 +0700

    Start up#0.2

---
+++ ui/colors.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+class AppColor {
+  static Color NamePage = const Color(0XFF000000);
+  static Color NameWidget = const Color(0XFF737272);
+  static Color Gradient1 = const Color(0XFF406AFF);
+  static Color Gradient2 = const Color(0XFFD842DB);
+  static Color Gradient3 = const Color(0XFFe43a15);
+  static Color Gradient4 = const Color(0XFFe65245);
+  static Color Gradient5 = const Color(0XFF757F9A);
+  static Color Gradient6 = const Color(0XFFD7DDE8);
+  static Color Gradient7 = const Color(0XFFe52d27);
+  static Color Gradient8 = const Color(0XFFb31217);
+  static Color WidgetBackground = const Color(0XFFE9EBFF);
+  static Color Font_sub = const Color(0XFF737272);
+  static Color box_class = const Color(0XFFE7E7E7);
+  static Color offline = const Color(0XFFC4C4C4);
+  static Color background_textfield = const Color(0XFF928D8D);
+}
