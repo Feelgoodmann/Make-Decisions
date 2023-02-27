c67780ed32740a24a11855bba3a3858b96815ab8
Feelgoodmann <tanapat.ch@kkumail.com>
2023-02-27 18:48:21 +0700

    foodpic

--- lib/resources/foodInfo.dart
+++ lib/resources/foodInfo.dart
@@ -14,73 +14,73 @@ class FoodResInfoState extends State<FoodResInfo>{
   var foodResInfo = {
     1: {
       "type": "อาหารตามสั่ง",
-      "imageUrl": "",
+      "imageUrl": "assets/images/food1.png",
       "food": ["ข้าวผัด", "ผัดกะเพรา", "ผัดพริกแกง"],
     },
 
     2: {
       "type": "อาหารเส้น",
-      "imageUrl": "",
+      "imageUrl": "assets/images/food2.png",
       "food": ["ก๋วยเตี๋ยว", "ราเมน", "ขนมจีน"],
     },
 
     3: {
       "type": "ฟาสฟู้ต",
-      "imageUrl": "",
+      "imageUrl": "assets/images/food3.png",
       "food": ["พิซซ่า", "ไก่ทอด", "แฮมเบอเกอร์"],
     },
 
     4: {
       "type": "หมูกระทะ",
-      "imageUrl": "",
+      "imageUrl": "assets/images/food4.png",
       "food": [],
     },
 
     5: {
       "type": "อาหารริมทาง",
-      "imageUrl": "",
+      "imageUrl": "assets/images/food5.png",
       "food": ["ลูกชิ้นทอด", "โรตี", "น้ำปั่น"],
     },
 
     6: {
       "type": "อาหารเพื่อสุขภาพ",
-      "imageUrl": "",
+      "imageUrl": "assets/images/food6.png",
       "food": ["อกไก่", "สลัด"],
     },
 
     7: {
       "type": "ชาบู",
-      "imageUrl": "",
+      "imageUrl": "assets/images/food7.png",
       "food": [],
     },
 
     8: {
       "type": "อาหารอิสาน",
-      "imageUrl": "",
+      "imageUrl": "assets/images/food8.png",
       "food": ["ส้มตำ", "ไก่ย่าง", "ลาบ"],
     },
 
     9: {
       "type": "อาหารจีน",
-      "imageUrl": "",
+      "imageUrl": "assets/images/food9.png",
       "food": ["โต๊ะจีน", "เป็ดปักกิ่ง", "ติ่มซำ"],
     },
 
     10: {
       "type": "อาหารเกาหลี",
-      "imageUrl": "",
+      "imageUrl": "assets/images/food10.png",
       "food": ["คิมบับ", "เนื้อย่าง", "ไก่เกาหลี"],
     },
 
     11: {
       "type": "อาหารญี่ปุ่น",
-      "imageUrl": "",
+      "imageUrl": "assets/images/food11.png",
       "food": ["ซูชิ", "ข้าวด้ง", "ราเมน"],
     },
 
     12: {
       "type": "อาหารอินเดีย",
-      "imageUrl": "",
+      "imageUrl": "assets/images/food12.png",
       "food": ["นาน", "ไก่ทันดูรี"],
     },
 
