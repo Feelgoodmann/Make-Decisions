57442625525490b0ed8ecf39ba67294c5df8b00e
ThanawitTapabud <thanawit.t@kkumail.com>
2023-04-06 01:06:36 +0700

    spinCanUse

--- lib/screens/spin_bilnd.dart
+++ lib/screens/spin_bilnd.dart
@@ -14,6 +14,7 @@ class SpinBilndWheel extends StatefulWidget {
 class _SpinBilndWheelState extends State<SpinBilndWheel> {
   StreamController<int> selected = StreamController<int>();
   final choiceField = TextEditingController();
+  int selectedValue = 0;
   List<String> choices = [
     "หมูน้อย",
     "ชิวเล้า",
@@ -33,10 +34,12 @@ class _SpinBilndWheelState extends State<SpinBilndWheel> {
     Colors.blueGrey,
   ];
 
-  void handleRoll() {
-    selected.add(
-      roll(choices.length),
-    );
+  void handleRoll() async {
+    final index = roll(choices.length);
+    selected.add(index);
+    setState(() {
+      selectedValue = index;
+    });
   }
 
   @override
@@ -57,6 +60,48 @@ class _SpinBilndWheelState extends State<SpinBilndWheel> {
     });
   }
 
+  void showSelectedItem() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Container(
+            width: 100.0,
+            height: 100.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                normalText('${choices[selectedValue]}', notblack),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+            ElevatedButton(
+              onPressed: (choices.length > 2)
+                  ? () {
+                      setState(() {
+                        choices.removeAt(selectedValue);
+                        // reset the selected value to the first item
+                        selectedValue = 0;
+                      });
+                      Navigator.of(context).pop();
+                    }
+                  : null,
+              child: const Text('Remove'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,6 +114,7 @@ class _SpinBilndWheelState extends State<SpinBilndWheel> {
                 child: SingleChildScrollView(
                   child: Column(
                     children: [
+                      const SizedBox(height: 40),
                       headerText("หมุนวงล้อ", notblack),
                       Container(
                         height: 300.0,
@@ -86,6 +132,7 @@ class _SpinBilndWheelState extends State<SpinBilndWheel> {
                               setState(() {
                                 isAnimating = false;
                               });
+                              showSelectedItem();
                             },
                             selected: selected.stream,
                             indicators: const <FortuneIndicator>[
@@ -123,7 +170,7 @@ class _SpinBilndWheelState extends State<SpinBilndWheel> {
                             Column(
                               children: [
                                 normalText("เพิ่มตัวเลือกในการสุ่ม", notblack),
-                                const SizedBox(height: 10.0),
+                                const SizedBox(height: 5),
                                 Container(
                                   margin: const EdgeInsets.symmetric(
                                       horizontal: 10.0),
@@ -148,23 +195,48 @@ class _SpinBilndWheelState extends State<SpinBilndWheel> {
                                               ? removeChoice(value)
                                               : null,
                                           child: Chip(
-                                            label: Text(value,
-                                              style: TextStyle(
-                                                fontSize: 16
-                                              ),
+                                            label: Row(
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: [
+                                                Text(
+                                                  value,
+                                                  style:
+                                                    const TextStyle(fontSize: 16),
+                                                ),
+                                                const SizedBox(width: 3),
+                                                const CircleAvatar(
+                                                  backgroundColor: Color(0xffC85050),
+                                                  radius: 9,
+                                                  child: Icon(
+                                                    Icons.close,
+                                                    color: Colors.white,
+                                                    size: 11,
+                                                  ),
+                                                ),
+                                              ],
                                             ),
-                                            deleteIcon: Icon(Icons.clear),
+                                            deleteIcon: const Icon(Icons.clear),
                                           ),
                                         );
                                       }).toList(),
                                     ),
                                   ),
                                 ),
-                                const SizedBox(height: 10.0),
+                                const SizedBox(height: 5),
+                                Row(
+                                  children: [
+                                    Padding(padding: EdgeInsets.only(right: 235)),
+                                    Text('Limit: 12',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold
+                                      ),
+                                    )
+                                  ],
+                                ),
                                 Row(
                                   children: [
                                     Container(
-                                      height: 50,
+                                      height: 40,
                                       width: 140,
                                       padding:
                                           const EdgeInsets.only(left: 10.0),
@@ -190,10 +262,10 @@ class _SpinBilndWheelState extends State<SpinBilndWheel> {
                                             filled: true),
                                       ),
                                     ),
-                                    const SizedBox(width: 10.0),    
+                                    const SizedBox(width: 10.0),
                                     spinText(":", 30.0),
                                     Container(
-                                      height: 50,
+                                      height: 40,
                                       width: 60,
                                       padding:
                                           const EdgeInsets.only(left: 10.0),
@@ -241,7 +313,8 @@ class _SpinBilndWheelState extends State<SpinBilndWheel> {
                                         onPressed: () {
                                           if (choiceField.text.isEmpty) {
                                             debugPrint("TextField empty");
-                                          } else if (choices.length == 12) {
+                                          } else if (choices.length == 12 ||
+                                              isAnimating == true) {
                                             null;
                                           } else {
                                             addChoice(choiceField.text);
@@ -262,7 +335,8 @@ class _SpinBilndWheelState extends State<SpinBilndWheel> {
                                 ),
                               ],
                             ),
-                            const SizedBox(height: 10.0),
+                            const SizedBox(height: 5),
+                            /*
                             Row(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
@@ -277,7 +351,8 @@ class _SpinBilndWheelState extends State<SpinBilndWheel> {
                                 smallText("สุ่มค่าไม่ซ้ำกัน", notblack),
                               ],
                             ),
-                            const SizedBox(height: 10.0),
+                            */
+                            //const SizedBox(height: 10.0),
                             RollButton(
                               onPressed: isAnimating ? null : handleRoll,
                               name: 'หมุนเลย',
@@ -303,189 +378,3 @@ class _SpinBilndWheelState extends State<SpinBilndWheel> {
     );
   }
 }
-/*
-class AddChoices extends StatefulWidget {
-  final void Function(List<String>) updateAddChoicesList;
-  final void Function(List<String>) updateRemoveChoicesList;
-  const AddChoices(
-      {Key? key,
-      required this.updateAddChoicesList,
-      required this.updateRemoveChoicesList})
-      : super(key: key);
-
-  @override
-  State<AddChoices> createState() => _AddChoicesState();
-}
-
-class _AddChoicesState extends State<AddChoices> {
-  final choiceField = TextEditingController();
-  final List<String> newChoices = [];
-  @override
-  void dispose() {
-    choiceField.dispose();
-    super.dispose();
-  }
-
-  void addChoices(String value) {
-    setState(() {
-      newChoices.add(value);
-      widget.updateAddChoicesList(newChoices);
-      choiceField.clear();
-    });
-  }
-
-  void removeChoices(String value) {
-    setState(() {
-      newChoices.remove(value);
-      widget.updateRemoveChoicesList(newChoices);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        normalText("เพิ่มตัวเลือกในการสุ่ม", notblack),
-        const SizedBox(height: 10.0),
-        Container(
-          padding: const EdgeInsets.only(right: 10.0, left: 10.0),
-          child: const TextField(
-            maxLines: 4,
-            decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  borderSide: BorderSide(
-                    width: 0,
-                    style: BorderStyle.none,
-                  ),
-                ),
-                hintText: 'ตัวเลือกจะแสดงตรงนี้ด้วย',
-                fillColor: Colors.white,
-                filled: true),
-          ),
-        ),
-        const SizedBox(height: 10.0),
-        Row(
-          children: [
-            Container(
-              height: 50,
-              width: 140,
-              padding: const EdgeInsets.only(left: 10.0),
-              child: TextField(
-                controller: choiceField,
-                maxLines: 1,
-                style: const TextStyle(fontSize: 15.0),
-                decoration: const InputDecoration(
-                    contentPadding:
-                        EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15)),
-                      borderSide: BorderSide(
-                        width: 0,
-                        style: BorderStyle.none,
-                      ),
-                    ),
-                    hintText: 'ใส่ตัวเลือก',
-                    fillColor: Colors.white,
-                    filled: true),
-              ),
-            ),
-            const SizedBox(width: 10.0),
-            spinText(":", 30.0),
-            Container(
-              height: 50,
-              width: 60,
-              padding: const EdgeInsets.only(left: 10.0),
-              child: const TextField(
-                maxLines: 1,
-                decoration: InputDecoration(
-                    contentPadding:
-                        EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15)),
-                      borderSide: BorderSide(
-                        width: 0,
-                        style: BorderStyle.none,
-                      ),
-                    ),
-                    fillColor: Colors.white,
-                    filled: true),
-              ),
-            ),
-            const SizedBox(width: 10.0),
-            spinText("%", 25.0),
-            Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topRight,
-                    end: Alignment.bottomLeft,
-                    colors: [
-                      Color(0xff50C878),
-                      Color(0xff3EC3A4),
-                    ]),
-                shape: BoxShape.circle,
-              ),
-              child: ElevatedButton(
-                child: const Icon(
-                  Icons.add,
-                  size: 30,
-                ),
-                onPressed: () {
-                  if (choiceField.text.isEmpty) {
-                    debugPrint("TextField empty");
-                  } else {
-                    addChoices(choiceField.text);
-                    choiceField.clear();
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: CircleBorder(),
-                  primary: Colors.transparent,
-                  onSurface: Colors.transparent,
-                  shadowColor: Colors.transparent,
-                  disabledBackgroundColor: Colors.transparent,
-                ),
-              ),
-            ),
-          ],
-        ),
-        SizedBox(height: 16),
-        Row(
-          children: [
-            Wrap(
-              spacing: 8,
-              runSpacing: 8,
-              children: newChoices.map((value) {
-                return GestureDetector(
-                  onTap: () => removeChoices(value),
-                  child: Chip(
-                    label: Text(value),
-                    deleteIcon: Icon(Icons.clear),
-                  ),
-                );
-              }).toList(),
-            ),
-          ],
-        )
-      ],
-    );
-  }
-}
-*/
-
-/*
-const TextField(
-        maxLines: 1,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(40)),
-            borderSide: BorderSide(
-              width: 0,
-              style: BorderStyle.none,
-            ),
-          ),
-          hintText: 'ใส่ตัวเลือก',
-          fillColor: Colors.white,
-          filled: true),
-        ),
-*/
